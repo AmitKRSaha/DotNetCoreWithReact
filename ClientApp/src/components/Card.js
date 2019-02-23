10236c0e3a4c81a26d83ba00d7d6972fe474a960
AKSAHA <sheol.aks@gmail.com>
2019-02-24 00:51:25 +0530

    Updated Card style

--- ClientApp/src/components/Card.js
+++ ClientApp/src/components/Card.js
@@ -3,12 +3,14 @@ import './Card.css';
 
 export const Card = (props) => {
     return (
-        <div className="info">
-            <img src={props.avatar_url} alt="MyImage" />
-            <div className="usrDetails">
-                <div>{props.name}</div>
-                <div>{props.company}</div>
 
+        <div style={{ margin: '1em' }}>
+            <img width="75" src={props.avatar_url} />
+            <div style={{ display: 'inline-block', marginLeft: 10 }}>
+                <div style={{ fontSize: '1.25em', fontWeight: 'bold' }}>
+                    {props.name}
+                </div>
+                <div>{props.company}</div>
             </div>
         </div>
     );
